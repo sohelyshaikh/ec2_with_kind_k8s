7ee5219a4b9a47ed5cd2828c1c9402af28d47088
EC2 Default User <ec2-user@ip-172-31-87-53.ec2.internal>
2022-05-28 19:00:00 +0000

    commented roles

--- ec2/role.tf
+++ ec2/role.tf
@@ -1,41 +1,41 @@
 
-resource "aws_iam_role_policy" "iam_policy" {
-  name = "ec2-policy"
-  role = aws_iam_role.ec2_role.id
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "ecr:*",
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      },
-    ]
-  })
+# resource "aws_iam_role_policy" "iam_policy" {
+#   name = "ec2-policy"
+#   role = aws_iam_role.ec2_role.id
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Action = [
+#           "ecr:*",
+#         ]
+#         Effect   = "Allow"
+#         Resource = "*"
+#       },
+#     ]
+#   })
 
-}
+# }
 
-resource "aws_iam_role" "ec2_role" {
-  name = "ec2-role"
+# resource "aws_iam_role" "ec2_role" {
+#   name = "ec2-role"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
-}
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Action = "sts:AssumeRole"
+#         Effect = "Allow"
+#         Sid    = ""
+#         Principal = {
+#           Service = "ec2.amazonaws.com"
+#         }
+#       },
+#     ]
+#   })
+# }
 
-resource "aws_iam_instance_profile" "ec2_profile" {
-  name = "ec2_profile"
-  role = aws_iam_role.ec2_role.name
-}
+# resource "aws_iam_instance_profile" "ec2_profile" {
+#   name = "ec2_profile"
+#   role = aws_iam_role.ec2_role.name
+# }
